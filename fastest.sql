9255d821ec90d1020354e7b04d2243e01fc6535c
gsmith <gsmith@grace.example.com>
2010-03-18 19:55:15 -0400

    Grouped fastest result list usefully

--- fastest.sql
+++ fastest.sql
@@ -1,5 +1,12 @@
 SELECT
   set,script,scale,clients,workers,
-  round(tps) as tps
-FROM tests
+  round(tps) AS tps
+FROM
+(
+  SELECT
+    set,script,scale,clients,workers,
+    max(tps) AS tps
+  FROM tests
+  GROUP BY set,script,scale,clients,workers
+) AS grouped
 ORDER BY tps DESC LIMIT 20;

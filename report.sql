952661fb9c0dce6c99d97a93f57891bc853dfd85
Greg Smith <greg@2ndQuadrant.com>
2013-09-22 05:10:14 -0400

    Add rate limit into output reports

--- report.sql
+++ report.sql
@@ -1,9 +1,9 @@
 SELECT
-  set,scale,test,script,clients,workers,
+  set,scale,test,script,clients,rate_limit,workers,
   round(tps) AS tps,
   round(1000*avg_latency)/1000 AS avg_latency,
   round(1000*percentile_90_latency)/1000 AS "90%<",
   1000*round(max_latency)/1000 AS max_latency,
   trans
 FROM tests
-ORDER BY set,scale,script,clients,test; 
+ORDER BY set,scale,script,clients,rate_limit,test; 

2a3c822b92e8c04bda5c5472177d123c811e922b
Greg Smith <gregs1104@gmail.com>
2020-05-07 13:09:30 -0400

    Fix random generation range on PG9.6+ test scripts. Make more of the report scripts executable.

--- tests/insert.sql
+++ tests/insert.sql
@@ -1,6 +1,3 @@
-\set nbranches :scale
-\set ntellers 10 * :scale
-\set naccounts 100000 * :scale
 \set aid random(1, 100000 * :scale)
 \set bid random(1, 1 * :scale)
 \set tid random(1, 10 * :scale)
--- tests/select.sql
+++ tests/select.sql
@@ -1,3 +1,2 @@
-\set naccounts 100000 * :scale
-\set aid random(1, 100000 * :naccounts)
+\set aid random(1, 100000 * :scale)
 SELECT abalance FROM pgbench_accounts WHERE aid = :aid;

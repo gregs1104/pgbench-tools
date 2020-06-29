230eafc6daa13baaac29a06f99066e3afd7af82f
Greg Smith <gregs1104@gmail.com>
2020-06-29 17:00:36 -0400

    Tweak tps per client report

--- reports/tps-per-client.sql
+++ reports/tps-per-client.sql
@@ -5,8 +5,9 @@ SELECT
   clients,rate_limit,
   round(avg(tps)) as tps,
   round(avg(tps)/clients) as tps_per_client,
-  round(1000 * avg(max_latency))/1000 AS max_latency ,
-  to_char(avg(end_time -  start_time),'HH24:MI:SS') AS runtime
+  round(1000 * avg(percentile_90_latency))/1000 AS "90%<",
+  round(1000 * avg(max_latency))/1000 AS max_latency
+--  to_char(avg(end_time -  start_time),'HH24:MI:SS') AS runtime
 FROM tests 
 GROUP BY set,script,scale,clients,rate_limit
 ORDER BY set,script,scale,clients,rate_limit; 

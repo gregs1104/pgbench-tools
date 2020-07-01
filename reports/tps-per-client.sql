eefe204e5307eeb1c464eeb7dff0d1c292725c2a
Greg Smith <gregs1104@gmail.com>
2020-07-01 10:26:38 -0400

    Tighten tps-per-client to use less columns. This might be confusing with a rate limited output. I don't expect most tps per client investigations to be rate limited runs.

--- reports/tps-per-client.sql
+++ reports/tps-per-client.sql
@@ -2,11 +2,12 @@ SELECT
   set,script,scale,
   pg_size_pretty(avg(dbsize)::int8) AS db_size,
 --  pg_size_pretty(avg(dbsize)::int8 / scale) AS size_per_scale,
-  clients,rate_limit,
+  clients,
+--  rate_limit,
   round(avg(tps)) as tps,
-  round(avg(tps)/clients) as tps_per_client,
+  round(avg(tps)/clients) as per_cl,
   round(1000 * avg(percentile_90_latency))/1000 AS "90%<",
-  round(1000 * avg(max_latency))/1000 AS max_latency
+  round(1000 * avg(max_latency))/1000 AS max_lat
 --  to_char(avg(end_time -  start_time),'HH24:MI:SS') AS runtime
 FROM tests 
 GROUP BY set,script,scale,clients,rate_limit

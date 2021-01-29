92e19434a8ce4828a11da1935dc7d8b6a3034f01
Greg Smith <gregs1104@gmail.com>
2021-01-29 07:13:24 -0500

    Add pg_stat_statements snapshot to results.

--- init/resultdb.sql
+++ init/resultdb.sql
@@ -144,6 +144,41 @@ CREATE TABLE test_settings (
     numeric_unit text
 );
 
+CREATE TABLE test_statements (
+    server text,
+    test text,
+    queryid bigint,
+    query text,
+    plans bigint,
+    total_plan_time double precision,
+    min_plan_time double precision,
+    max_plan_time double precision,
+    mean_plan_time double precision,
+    stddev_plan_time double precision,
+    calls bigint,
+    total_exec_time double precision,
+    min_exec_time double precision,
+    max_exec_time double precision,
+    mean_exec_time double precision,
+    stddev_exec_time double precision,
+    rows bigint,
+    shared_blks_hit bigint,
+    shared_blks_read bigint,
+    shared_blks_dirtied bigint,
+    shared_blks_written bigint,
+    local_blks_hit bigint,
+    local_blks_read bigint,
+    local_blks_dirtied bigint,
+    local_blks_written bigint,
+    temp_blks_read bigint,
+    temp_blks_written bigint,
+    blk_read_time double precision,
+    blk_write_time double precision,
+    wal_records bigint,
+    wal_fpi bigint,
+    wal_bytes numeric
+);
+
 CREATE INDEX idx_test_metrics_test on test_metrics_data(server,test);
 
 CREATE UNIQUE INDEX idx_server_set on testset(server,set);
--- prequel.sql
+++ prequel.sql
@@ -131,3 +131,38 @@ CREATE TABLE test_settings (
     numeric_value numeric,
     numeric_unit text
 );
+
+CREATE TABLE test_statements (
+    server text,
+    test text,
+    queryid bigint,
+    query text,
+    plans bigint,
+    total_plan_time double precision,
+    min_plan_time double precision,
+    max_plan_time double precision,
+    mean_plan_time double precision,
+    stddev_plan_time double precision,
+    calls bigint,
+    total_exec_time double precision,
+    min_exec_time double precision,
+    max_exec_time double precision,
+    mean_exec_time double precision,
+    stddev_exec_time double precision,
+    rows bigint,
+    shared_blks_hit bigint,
+    shared_blks_read bigint,
+    shared_blks_dirtied bigint,
+    shared_blks_written bigint,
+    local_blks_hit bigint,
+    local_blks_read bigint,
+    local_blks_dirtied bigint,
+    local_blks_written bigint,
+    temp_blks_read bigint,
+    temp_blks_written bigint,
+    blk_read_time double precision,
+    blk_write_time double precision,
+    wal_records bigint,
+    wal_fpi bigint,
+    wal_bytes numeric
+);

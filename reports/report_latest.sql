15c13b9fe002af79e08e464ca47dd0859be484c9
Greg Smith <gregs1104@gmail.com>
2022-10-01 18:16:16 -0400

    Change latest set report to show human DB size. Transaction count was removed to make it fit.

--- reports/report_latest.sql
+++ reports/report_latest.sql
@@ -2,14 +2,15 @@ WITH lastset AS (
   SELECT max(set) as lastset from tests
 )
 SELECT
-  set,script,scale,test,clients,multi,
+  set,script,scale,
+  pg_size_pretty(dbsize::int8) AS db_size,
+  test,clients,multi,
   rate_limit AS limit,
   client_limit AS client_limit,
   round(tps) AS tps,
   round(1000*avg_latency)/1000 AS avg_latency,
   round(1000*percentile_90_latency)/1000 AS "90%<",
-  1000*round(max_latency)/1000 AS max_latency,
-  trans
+  1000*round(max_latency)/1000 AS max_latency
 FROM tests
 WHERE set = (SELECT lastset FROM lastset)
 ORDER BY set,script,scale,clients,rate_limit,multi,test; 

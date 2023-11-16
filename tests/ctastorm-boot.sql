2287e68907d8a8c506125e15d9ce5e3720ac1e20
Greg Smith <gregs1104@gmail.com>
2023-11-16 15:25:57 -0500

    Add CTAStorm, a multiple concurrent CREATE TABLE AS workload. CTAS makes for a very fast 100% write benchmark.
    Example run:
    
        psql -d cbc -f ctastorm-boot.sql
        pgbench -n -c 4 -t 4 -f ctastorm.sql cbc
    
    Linux storage rated for 6.5GB/s writes validated to 6343MB/s.
    MacBook M1 Pro storage tested to peak at 5745MB/s on checkpoints.

---
+++ tests/ctastorm-boot.sql
@@ -0,0 +1,9 @@
+\set SIZEGB :scale
+CREATE EXTENSION pg_stat_statements;
+DROP TABLE sl_b;
+SELECT pg_stat_reset();
+SELECT pg_stat_statements_reset();
+\timing on
+\set ECHO queries
+CREATE TABLE sl_b AS SELECT gs.x AS seq,pgs.* FROM generate_series(0, :SIZEGB * 12500,1) gs(x) LEFT JOIN pg_settings pgs ON (true);
+VACUUM (FREEZE ON, ANALYZE ON, VERBOSE ON) sl_b;
---
+++ tests/ctastorm.sql
@@ -0,0 +1,3 @@
+CREATE TABLE sl_:client_id AS SELECT * FROM sl_b;
+DROP TABLE sl_:client_id;
+

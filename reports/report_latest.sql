b42ad52b384d7d4de97e7e4233bacad046544ad4
Greg Smith <gregs1104@gmail.com>
2023-12-14 20:59:31 -0500

    Scale latest run report with conistent rounding, save space.

--- reports/report_latest.sql
+++ reports/report_latest.sql
@@ -8,9 +8,9 @@ SELECT
   rate_limit AS limit,
   client_limit AS client_limit,
   round(tps) AS tps,
-  round(1000*avg_latency)/1000 AS avg_latency,
-  round(1000*percentile_90_latency)/1000 AS "90%<",
-  1000*round(max_latency)/1000 AS max_latency
+  1000*round(1000*avg_latency)/1000 AS avg_latency,
+  1000*round(1000*percentile_90_latency)/1000 AS "90%<",
+  1000*round(1000*max_latency)/1000 AS max_latency
 FROM tests
 WHERE set = (SELECT lastset FROM lastset)
 ORDER BY set,script,scale,clients,rate_limit,multi,test; 

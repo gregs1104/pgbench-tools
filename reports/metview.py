b648cbe5e06080f5055872f9b7174df6a51b8e81
Greg Smith <gregs1104@gmail.com>
2024-02-27 09:22:50 -0500

    Streamline metview to functional set with documented TODOs.

--- reports/metview.py
+++ reports/metview.py
@@ -5,34 +5,75 @@ metview.py generates metrics graphs out of a pgbench-tools benchmark results
 database.  This is the successor to the osm-metrics.py example code.
 
 This code is at rough works for me quality with a minimal UI.
-Committing and releasing in this state as a safety net to enable a
-major refactoring to make this a proper CLI tool.
+Committing and releasing in this state as a safety net to enable
+refactoring toward a proper CLI tool.
 """
 
+import argparse
 import os
-import sys
-import psycopg2
-import psycopg2.extras
-from datetime import datetime
-
-import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib
 import matplotlib.image as image
 from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
-import argparse
+import pandas as pd
+import psycopg2
+import psycopg2.extras
 
-def query_multimet(options):
+def connect(options):
+    # TODO put these parameters into options
+    conn_string = "host='localhost' dbname='results' user='gsmith' password='secret'"
+    print("Connecting to database\n	->%s" % (conn_string))
+    return psycopg2.connect(conn_string)
+
+def parse():
+    parser = argparse.ArgumentParser(description='metview.py benchmark results metrics viewer')
+    parser.add_argument("server", help="server name",nargs='?',default='rising')
+    parser.add_argument("test", type=int, help='Test number',nargs='?',default=4974)
+    return vars(parser.parse_args())
+
+def images_dir(options):
+    base="images"
+    try:
+        os.mkdir(base)
+    except:
+        # TODO catch real errors, continue to ignore already exists error
+        pass
+    return base
+
+def gen_label(options,df):
     server=options['server']
     test=options['test']
 
-    diskdevs=["sda","sda"] # gp2/3
+    cpu=server
+    # TODO Lookup CPU info from server table
+    if server=='rising':
+        cpu='5950X'
 
-    server_label = server
-    col='collected'
+    # Extract run metadata from first row
+    clients=df.iloc[0]['clients']
+
+    try:
+        rate_limit=round(df.iloc[0]['rate_limit'])
+    except:
+        rate_limit=round(df.iloc[0]['tps'])
+
+    db_gb=round(df.iloc[0]['db_gb'])
+    script=df.iloc[0]['script'].upper()
+
+    view_label=cpu+" "+script+" "+str(db_gb)+"GB "+str(clients)+" clients @ "+str(rate_limit)+" TPS"
+    return view_label
+
+def gen_file_name(base,view,server,test):
+    unslashed=view.replace("/","-")
+    name=os.path.join(base,server+"-"+str(test)+"-"+unslashed)
+    return name
+
+def query_multi_met(options):
+    server=options['server']
+    test=options['test']
+    # TODO determine dbagg based on length of teat run
     dbagg='second'
 
-    # TODO Use SQL proof parameter substitution here instead of Python's
+    # TODO Use SQL injection proof parameter substitution here instead of Python's
     sql="""
     SELECT
       --test_metrics_data.server,
@@ -57,81 +98,12 @@ def query_multimet(options):
             'rate','avg_latency','min_latency','max_latency',
             'min_schedule_lag_ms','avg_schedule_lag_ms','max_schedule_lag_ms',
             'pg_clients_active','pg_clients_idle','pg_db_size','pg_max_query_runtime_sec',
-    --      'bi','bo'
-    --      'id','wa',
           'Dirty'
-    --      '%s_rMB/s',
-    --      '%s_wMB/s',
-    --      '%s_rMB/s',
-    --      '%s_wMB/s',
-    --      '%s_MB/s',
-    --      '%s_MB/s'
           )
     GROUP BY test_metrics_data.server,script,scale,clients,rate_limit,tps,round(dbsize / (1024*1024*1024)),metric,date_trunc('%s',collected)
     ORDER BY test_metrics_data.server,script,scale,clients,rate_limit,round(dbsize / (1024*1024*1024)),metric,date_trunc('%s',collected)
-    ;""" % (dbagg,test,server,
-        # Linux R/W
-        diskdevs[0],diskdevs[0],
-        diskdevs[1],diskdevs[1],
-        # Mac total
-        diskdevs[0],diskdevs[1],
-        dbagg,dbagg)
-
+    ;""" % (dbagg,test,server,dbagg,dbagg)
     return sql
-
-# TODO Turn each of these metric set groups into their own function
-def metric_set_groups(options):
-    server=options['server']
-    test=options['test']
-
-    view=['min_schedule_lag_ms','avg_schedule_lag_ms','max_schedule_lag_ms']
-    view_label='Schedule Lag Latency'
-    ylabel="Lag"
-
-    view=['min_latency','max_latency','avg_latency']
-    ylabel="Latency (ms)"
-    #view_label='Latency '+cpu+" "+test
-    view_label='Latency '+str(test)
-
-    view=('Dirty')
-    view_label='Dirty'
-    ylabel="Dirty"
-
-
-def images_dir(options):
-    base="images"
-    try:
-        os.mkdir(base)
-    except:
-        pass
-    return base
-
-def create_label(options,df):
-    server=options['server']
-    test=options['test']
-
-    cpu=server
-    if server=='rising':
-        cpu='5950X'
-
-    # Extract run metadata from first row
-    clients=df.iloc[0]['clients']
-
-    try:
-        rate_limit=round(df.iloc[0]['rate_limit'])
-    except:
-        rate_limit=round(df.iloc[0]['tps'])
-
-    db_gb=round(df.iloc[0]['db_gb'])
-    script=df.iloc[0]['script'].upper()
-
-    view_label=cpu+" "+script+" "+str(db_gb)+"GB "+str(clients)+" clients @ "+str(rate_limit)+" TPS"
-    return view_label
-
-def gen_file_name(base,view,server,test):
-    unslashed=view.replace("/","-")
-    name=os.path.join(base,server+"-"+str(test)+"-"+unslashed)
-    return name
 
 def graph_group(options,df):
     server=options['server']
@@ -149,7 +121,7 @@ def graph_group(options,df):
     view_label='Rate'
     ylabel="TPS"
 
-    view_label=create_label(options,df)
+    view_label=gen_label(options,df)
 
     g=df.groupby('metric')
 
@@ -178,27 +150,30 @@ def graph_group(options,df):
                 ylabel="Dirty Memory MB"
 
             ax=v['avg'].plot(rot=90,title=view_label,figsize=(8,6))
-
             #,color=colors[rendered])
+
             # This just shows avg/avg/avg on legend
             # ax.legend()
 
             ax.set_ylabel(ylabel)
             ax.grid(True,which='both')
-            ab = AnnotationBbox(im, (1, 0), frameon=False, xycoords='axes fraction',
-                 box_alignment=(0.55,1.85))
-            ax.add_artist(ab)
 
             fn=gen_file_name(base,k,server,test)
             # Only save on last metric in the view list
             if rendered==(len(view)):
+                # TODO Bottom part of graph is strangely cut off?  Rotation issue?
                 plt.savefig(fn,dpi=600)  # 80 for =640x480 figures
                 print("saved to '%s.png'" % fn)
 
+                ab = AnnotationBbox(im, (1, 0), frameon=False, xycoords='axes fraction',
+                     box_alignment=(0.55,1.85))
+                ax.add_artist(ab)
+                plt.savefig(fn+"-logo",dpi=600)  # 80 for =640x480 figures
+                print("saved to '%s-logo.png'" % fn)
 
-def process(options,conn):
+def graph(options,conn):
     try:
-        sql=query_multimet(options)
+        sql=query_multi_met(options)
         print(sql)
         df = pd.read_sql_query(sql, conn)
         print(df)
@@ -206,19 +181,10 @@ def process(options,conn):
     finally:
         conn.close()
 
-def connect(options):
-    conn_string = "host='localhost' dbname='results' user='gsmith' password='secret'"
-    print("Connecting to database\n	->%s" % (conn_string))
-    return psycopg2.connect(conn_string)
-
-def parse():
-    parser = argparse.ArgumentParser(description='metview.py benchmark results metrics viewer')
-    parser.add_argument("test", type=int, help='Test number',nargs='?',default=4974)
-    parser.add_argument("server", help="server name",nargs='?',default='rising')
-    args = parser.parse_args()
-    return args
+def gen_graphs():
+    args_dict=parse()
+    c=connect(args_dict)
+    graph(args_dict,c)
 
 if __name__ == "__main__":
-    args_dict=vars(parse())
-    c=connect(args_dict)
-    process(args_dict,c)
+    gen_graphs()

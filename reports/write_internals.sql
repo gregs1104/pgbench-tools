f4e14bbc1eb201655548c567c35ebe5e7ab80160
Greg Smith <gregs1104@gmail.com>
2025-02-16 14:11:21 -0500

    Add sample results submission script.

--- reports/write_internals.sql
+++ reports/write_internals.sql
@@ -1,6 +1,6 @@
 SELECT
+  tests.server || ' - ' || tests.set::text  || ' - ' || tests.test::text AS ref_info,
   to_char(end_time,'YYYY/MM/DD') AS run,
-  --tests.test,
   tests.server_cpu AS cpu,
   tests.server_mem_gb AS mem_gb,
   script,
@@ -10,23 +10,29 @@ SELECT
   scale,
   CASE WHEN jsonb_exists(artifacts, 'node_count')
     THEN (artifacts->'node_count')::numeric ELSE 0 END AS nodes,
-  pg_size_pretty(dbsize) AS dbsize,
+  round(dbsize / 1024 / 1024 / 1024,1) AS db_gb,
   tps,  percentile_90_latency, rate_limit,
   round(extract(epoch from (tests.end_time - tests.start_time))/60/60,2) as hours,
   CASE WHEN jsonb_exists(artifacts, 'node_count') AND jsonb_exists(artifacts, 'overall')
     THEN round((artifacts->'node_count')::numeric / (artifacts->'overall')::numeric / 1000,0)
     ELSE 0 END AS nodes_kips,
   (
-  SELECT value FROM test_settings WHERE
+  SELECT round(numeric_value / 1024 / 1024 / 1024,1) FROM test_settings WHERE
     test_settings.server=tests.server AND test_settings.test=tests.test AND
     test_settings.name='shared_buffers'
     LIMIT 1
-  ) as shared,
-  (SELECT value FROM test_settings WHERE
+  ) as shared_gb,
+  (SELECT round(numeric_value / 1024 / 1024 / 1024,1) FROM test_settings WHERE
     test_settings.server=tests.server AND test_settings.test=tests.test AND
     test_settings.name='maintenance_work_mem'
     LIMIT 1
-  ) as maint,
+  ) as maint_gb,
+  (
+  SELECT round(test_settings.numeric_value / 1024 / 1024 / 1024,1) FROM test_settings WHERE
+    test_settings.server=tests.server AND test_settings.test=tests.test AND
+    test_settings.name='max_wal_size'
+    LIMIT 1
+  ) as max_wal_gb,
   (
   SELECT test_settings.setting FROM test_settings WHERE
     test_settings.server=tests.server AND test_settings.test=tests.test AND
@@ -34,20 +40,17 @@ SELECT
     LIMIT 1
   ) as fsync,
   (
-  SELECT pg_size_pretty(test_settings.setting::int8 * 1024 * 1024) FROM test_settings WHERE
-    test_settings.server=tests.server AND test_settings.test=tests.test AND
-    test_settings.name='max_wal_size'
-    LIMIT 1
-  ) as max_wal,
-  (
   SELECT test_settings.setting::integer / 60 FROM test_settings WHERE
     test_settings.server=tests.server AND test_settings.test=tests.test AND
     test_settings.name='checkpoint_timeout'
     LIMIT 1
   ) as timeout,
   round(extract(epoch from (tests.end_time - tests.start_time)) / (test_bgwriter.checkpoints_timed + test_bgwriter.checkpoints_req) / 60,1) as chkp_mins,
-  pg_size_pretty(round(60*60*buffers_checkpoint * 8192 / extract(epoch from (tests.end_time - tests.start_time)))::bigint) as chkp_bph,
-  pg_size_pretty(round(60*60*buffers_clean * 8192 / extract(epoch from (tests.end_time - tests.start_time)))::bigint) as clean_bph,
+  round(60*60*buffers_checkpoint * 8192 / extract(epoch from (tests.end_time - tests.start_time)) / 1024 / 1024) as chkp_mbph,
+  round(60*60*buffers_clean * 8192 / extract(epoch from (tests.end_time - tests.start_time)) / 1024 / 1024) as clean_mbph,
+
+--pg_size_pretty(round(60*60*buffers_checkpoint * 8192 / extract(epoch from (tests.end_time - tests.start_time)))::bigint) as chkp_bph,
+--pg_size_pretty(round(60*60*buffers_clean * 8192 / extract(epoch from (tests.end_time - tests.start_time)))::bigint) as clean_bph,
 --  pg_size_pretty(round(buffers_checkpoint * 8192 / extract(epoch from (tests.end_time - tests.start_time)))::bigint) as chkp_bytes_per_sec,
 --  60*60*(test_bgwriter.checkpoints_timed + test_bgwriter.checkpoints_req) / extract(epoch from (tests.end_time - tests.start_time))::bigint as chkp_per_hour,
 --  test_bgwriter.checkpoints_timed + test_bgwriter.checkpoints_req as chkpts,
@@ -59,9 +62,10 @@ SELECT
     100
   END AS timed_pct,
   round(100.0 * blks_hit / (blks_read + blks_hit)) as hit_pct,
-  round(blks_hit * 8192 / extract(epoch from (tests.end_time - tests.start_time)) / 1024 / 1024 / 1024,1) AS hit_gbps,
-  round(blks_read * 8192 / extract(epoch from (tests.end_time - tests.start_time)) / 1024 / 1024 / 1024,1) AS read_gbps,
-  pg_size_pretty(round(wal_written / extract(epoch from (tests.end_time - tests.start_time)))::bigint) AS wal_bps,
+  round(blks_hit * 8192 / extract(epoch from (tests.end_time - tests.start_time)) / 1024 / 1024,1) AS hit_mbps,
+  round(blks_read * 8192 / extract(epoch from (tests.end_time - tests.start_time)) / 1024 / 1024,1) AS read_mbps,
+--  pg_size_pretty(round(wal_written / extract(epoch from (tests.end_time - tests.start_time)))::bigint) AS wal_bps,
+  round(wal_written / 1024 / 1024 / extract(epoch from (tests.end_time - tests.start_time)),1) AS wal_mbps,
   (
   SELECT round(avg(test_metrics_data.value)) FROM test_metrics_data
   WHERE
@@ -89,4 +93,4 @@ WHERE
 ORDER BY tests.server,tests.server_cpu,tests.server_mem_gb,
   script,
   server_version,tests.set,
-  multi,scale,fsync,shared,maint,max_wal,timeout,extract(epoch from (tests.end_time - tests.start_time)) desc;
+  multi,scale,fsync,shared_gb,maint_gb,max_wal_gb,timeout,extract(epoch from (tests.end_time - tests.start_time)) desc;

701a9dd332bf27728874223c4161b77ffd8e85ca
Greg Smith <greg.smith@crunchydata.com>
2025-07-13 13:27:03 -0400

    Refactor metrics viewer to use metrics scaling table.

--- metview.py
+++ metview.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-metview.py generates metrics graphs out of a pgbench-tools benchmark results
+metview.py generates metrics graphs out of a pgbent benchmark results
 database.  This is the successor to the osm-metrics.py example code.
 
 This code is at rough works for me quality with a minimal UI.
@@ -18,39 +18,6 @@ import pandas as pd
 import psycopg2
 import psycopg2.extras
 
-# Define what categories various metrics are in
-rates=("rate")
-latencies=("avg_latency","max_latency ","min_latency")
-lags=("min_schedule_lag_ms","avg_schedule_lag_ms","max_schedule_lag_ms")
-
-linux_memory=("Active","Active(anon)","Active(file)","AnonHugePages",
-              "AnonPages","Bounce","Buffers","Cached","CmaFree","CmaTotal",
-              "CommitLimit","Committed_AS","DirectMap1G","DirectMap2M",
-              "DirectMap4k","Dirty","FileHugePages","FilePmdMapped",
-              "HardwareCorrupted","HugePages_Free","HugePages_Rsvd",
-              "HugePages_Surp","HugePages_Total","Hugepagesize","Hugetlb",
-              "Inactive","Inactive(anon)","Inactive(file)","KReclaimable",
-              "KernelStack","Mapped","MemAvailable","MemFree","MemTotal",
-              "Mlocked","NFS_Unstable","PageTables","Percpu","SReclaimable",
-              "SUnreclaim","Shmem","ShmemHugePages","ShmemPmdMapped","Slab",
-              "SwapCached","SwapFree","SwapTotal","Unevictable",
-              "VmallocChunk","VmallocTotal","VmallocUsed","Writeback",
-              "WritebackTmp", "Zswap","Zswapped")
-
-linux_vmstat=("b","bi","bo","buff","cache","cs","free","id","in","r",
-              "si","so","st","swpd","sy","us","wa")
-
-linux_iostat=("_%drqm","_%rrqm","_%util","_%wrqm",
-              "_aqu-sz","_d/s","_dMB/s",
-              "_d_await","_dareq-sz","_drqm/s",
-              "_r/s","_rMB/s","_r_await",
-              "_rareq-sz","_rrqm/s","_w/s",
-              "_wMB/s","_w_await","_wareq-sz",
-              "_wrqm/s")
-
-pg_stats=("pg_clients_active","pg_clients_idle","pg_db_size",
-          "pg_max_query_runtime_sec")
-
 def connect(options):
     # TODO Put database connection parameters into options
     conn_string = "host='localhost' dbname='results' user='gsmith' password='secret'"
@@ -59,8 +26,8 @@ def connect(options):
 
 def parse():
     parser = argparse.ArgumentParser(description='metview.py benchmark results metrics viewer')
-    parser.add_argument("server", help="server name",nargs='?',default='rising')
-    parser.add_argument("test", type=int, help='Test number',nargs='?',default=4974)
+    parser.add_argument("server", help="server name",nargs='?',default='twilight')
+    parser.add_argument("test", type=int, help='Test number',nargs='?',default=2568)
     return vars(parser.parse_args())
 
 def images_dir(options):
@@ -79,23 +46,22 @@ def gen_label(options,df):
     server=options['server']
     test=options['test']
 
-    cpu=server
-    # TODO Lookup CPU info from server table
-    if server=='rising':
-        cpu='5950X'
-
     # Extract run metadata from first row
+    cpu=df.iloc[0]['server_cpu']
     clients=df.iloc[0]['clients']
+    db_gb=round(df.iloc[0]['db_gb'])
+    script=df.iloc[0]['script'].upper()
 
     try:
         rate_limit=round(df.iloc[0]['rate_limit'])
     except:
         rate_limit=round(df.iloc[0]['tps'])
 
-    db_gb=round(df.iloc[0]['db_gb'])
-    script=df.iloc[0]['script'].upper()
+    if rate_limit>0:
+        view_label=cpu+" "+script+" "+str(db_gb)+"GB "+str(clients)+" clients @ "+str(rate_limit)+" TPS"
+    else:
+        view_label=cpu+" "+script+" "+str(db_gb)+"GB "+str(clients)+" clients"
 
-    view_label=cpu+" "+script+" "+str(db_gb)+"GB "+str(clients)+" clients @ "+str(rate_limit)+" TPS"
     return view_label
 
 def gen_file_name(base,view,server,test):
@@ -103,45 +69,119 @@ def gen_file_name(base,view,server,test):
     name=os.path.join(base,server+"-"+str(test)+"-"+unslashed)
     return name
 
-# TODO Create alternate query that includes all the metrics
-def query_multi_met(options):
+def gen_sql(options,dbagg):
     server=options['server']
     test=options['test']
-    # TODO determine dbagg based on length of test run
-    dbagg='second'
 
-    # TODO Use SQL injection proof parameter substitution here instead of Python's
+    # TODO Use SQL injection proof parameter substitution here instead of Python's?
     sql="""
     SELECT
       --test_metrics_data.server,
+      t.server_cpu AS server_cpu,
       script,
       tps,
       --scale,
       round(dbsize / (1024*1024*1024)) as db_gb,
       clients,
       rate_limit,
-      metric,
-      date_trunc('%s',collected) AS collected,
-      --min(value) AS min,
-      avg(value) AS avg,
-      max(value) AS max
-    FROM test_metrics_data,tests
-    WHERE
-      test_metrics_data.server=tests.server AND
-      test_metrics_data.test=tests.test AND
-      test_metrics_data.test=%s AND
-      test_metrics_data.server='%s' AND
-      metric IN (
-            'rate','avg_latency','min_latency','max_latency',
-            'min_schedule_lag_ms','avg_schedule_lag_ms','max_schedule_lag_ms',
-            'pg_clients_active','pg_clients_idle','pg_db_size','pg_max_query_runtime_sec',
-            'Dirty','Active','Cached'
-          )
-    GROUP BY test_metrics_data.server,script,scale,clients,rate_limit,tps,round(dbsize / (1024*1024*1024)),metric,date_trunc('%s',collected)
-    ORDER BY test_metrics_data.server,script,scale,clients,rate_limit,round(dbsize / (1024*1024*1024)),metric,date_trunc('%s',collected)
+      CASE WHEN mi.prefix='disk' THEN split_part(d.metric,'_',1) ELSE '' END AS disk,
+      CASE WHEN mi.metric_label IS null THEN d.metric ELSE mi.metric_label END AS metric,
+      mi.units,
+      mi.category AS cat,
+      mi.visibility AS vis,
+      date_trunc('%s',d.collected) AS collected,
+      CASE WHEN mi.multi IS null THEN min(d.value) ELSE min(d.value * mi.multi) END AS min,
+      CASE WHEN mi.multi IS null THEN avg(d.value) ELSE avg(d.value * mi.multi) END AS avg,
+      CASE WHEN mi.multi IS null THEN max(d.value) ELSE max(d.value * mi.multi) END AS max
+      FROM
+        tests t,test_metrics_data d
+      LEFT OUTER JOIN metrics_info mi ON
+        (d.metric=mi.metric OR (mi.prefix='disk' AND d.metric LIKE ('%%' || mi.metric)))
+      WHERE
+        d.server=t.server AND
+        d.test=t.test AND
+        (mi.uname=t.uname OR mi.uname IS null OR mi.uname='Database') AND
+        d.test=%s AND
+        d.server='%s'
+    GROUP BY d.server,t.server_cpu,script,scale,clients,rate_limit,tps,round(dbsize / (1024*1024*1024)),d.metric,mi.prefix,mi.metric_label,mi.units,mi.category,mi.multi,mi.visibility,date_trunc('%s',collected)
+    ORDER BY d.server,t.server_cpu,script,scale,clients,rate_limit,round(dbsize / (1024*1024*1024)),d.metric,mi.prefix,mi.metric_label,mi.units,mi.category,mi.multi,mi.visibility,date_trunc('%s',collected)
     ;""" % (dbagg,test,server,dbagg,dbagg)
+
     return sql
 
+def query_multi_met(options):
+    # TODO determine dbagg based on length of test run
+    dbagg='second'
+
+    sql=gen_sql(options, dbagg)
+    print ("sql=",sql)
+    return sql
+
+def query_single_met(options):
+    # TODO determine dbagg based on length of test run
+    dbagg='second'
+
+    sql=gen_sql(options, dbagg)
+    print ("sql=",sql)
+    return sql
+
+def graph_single(options,df):
+    server=options['server']
+    test=options['test']
+
+    metrics={}
+    rendered=0
+
+    base=images_dir(options)
+
+    plt.rcParams.update({'font.size':'18'})
+    colors=('green','blue','purple')
+
+    logo_file="reports/Color Horizontal.jpg"
+    logo=image.imread(logo_file)
+    logo_im = OffsetImage(logo, zoom=.03)
+
+    ylabel="TPS"
+
+    g=df.groupby(['metric'])
+
+    for k,v in g:
+        print("Processing",k)
+        print(v)
+        metrics[k]=v
+        v.set_index('collected',inplace=True)
+
+        view_label=gen_label(options,df)
+
+        # TODO put k into generated label
+        units=v['units'][0]
+        ylabel=units
+
+        metrics[k]=metrics[k].drop(columns=['avg','metric'])
+        metrics[k].rename(columns={'max': k}, inplace=True)
+
+        ax=v['avg'].plot(rot=90,title=view_label,figsize=(8,6))
+        #,color=colors[rendered])
+
+        # TODO This just shows avg/avg/avg on legend, should be min/avg/max
+        #ax.legend()
+
+        ax.set_ylabel(ylabel)
+        ax.grid(True,which='both')
+
+        fn=gen_file_name(base,k,server,test)
+        # TODO Bottom part of graph is strangely cut off?  Rotation issue?
+        plt.savefig(fn,dpi=600)  # 80 for =640x480 figures
+        print("saved to '%s.png'" % fn)
+
+        ab = AnnotationBbox(logo_im, (1, 0), frameon=False, xycoords='axes fraction',
+             box_alignment=(0.55,1.85))
+        ax.add_artist(ab)
+        plt.savefig(fn+"-logo",dpi=600)  # 80 for =640x480 figures
+        print("saved to '%s-logo.png'" % fn)
+        plt.clf()
+
+# This function combines multiple metrics onto a shared Y axis
 def graph_group(options,df):
     server=options['server']
     test=options['test']
@@ -158,18 +198,11 @@ def graph_group(options,df):
     logo=image.imread(logo_file)
     logo_im = OffsetImage(logo, zoom=.03)
 
-    # This function combines multiple metrics onto a shared Y axis
-    # TODO Break out the single metric use case to another function
-    if (True):
-        view_set=['min_latency','max_latency','avg_latency']
-        ylabel="Latency (ms)"
-        view_label='Latency '+str(test)
-    else:
-        view_set=['rate']
-        ylabel="TPS"
-        view_label=gen_label(options,df)
+    view_set=['min_latency','max_latency','avg_latency']
+    ylabel="Latency (ms)"
+    view_label='Latency '+str(test)
 
-    g=df.groupby('metric')
+    g=df.groupby(['cat','units','metric'])
 
     for k,v in g:
         print("Processing",k)
@@ -182,14 +215,6 @@ def graph_group(options,df):
 
         if k in view_set:
             rendered=rendered+1
-
-            if k in linux_memory:
-                # Linux mem figures are in KB, rescale
-                v['avg'] /= (1024 )
-                v['max'] /= (1024)
-                print("Reprocessed")
-                print(v)
-                ylabel="Memory MB"
 
             ax=v['avg'].plot(rot=90,title=view_label,figsize=(8,6))
             #,color=colors[rendered])
@@ -216,11 +241,11 @@ def graph_group(options,df):
 # TODO add options to change which query and graph function are called
 def graph(options,conn):
     try:
-        sql=query_multi_met(options)
+        sql=query_single_met(options)
         print(sql)
         df = pd.read_sql_query(sql, conn)
         print(df)
-        graph_group(options,df)
+        graph_single(options,df)
     finally:
         conn.close()
 
